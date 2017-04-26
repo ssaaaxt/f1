0f3677e02b69975000556bf6ca17b3e0b13da807
Administrator <Administrator@30.39.22.128>
2017-04-26 16:40:14 +0800

    colortest

---
+++ src/com/ssaaaxt/f1/test/ColorTest.java
@@ -0,0 +1,13 @@
+package com.ssaaaxt.f1.test;
+
+import org.junit.Test;
+
+public class ColorTest {
+
+	public String str;
+	
+	@Test
+	public void addStr(){
+		System.out.println(str+"123");
+	}
+}
