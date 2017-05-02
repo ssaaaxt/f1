4013653483a99e78c5f07af55189e17747db18c7
Administrator <Administrator@30.39.46.27>
2017-05-02 10:45:11 +0800

    添加 test.java

---
+++ src/com/ssaaaxt/f1/test/Test.java
@@ -0,0 +1,18 @@
+package com.ssaaaxt.f1.test;
+
+import org.junit.Test;
+
+public class ColorTest {
+
+	public String str;
+	
+	@Test
+	public void addStr() {
+		System.out.println(str+"123");
+	}
+	
+	@Test
+	public void addFun() {
+		
+	}
+}
