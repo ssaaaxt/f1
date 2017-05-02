2d910ad9b52341560a6fdc892f4d9b47beeb565d
Administrator <Administrator@30.39.46.27>
2017-05-02 10:03:50 +0800

    Bug分支

---
+++ src/com/ssaaaxt/f1/test/Bug.java
@@ -0,0 +1,15 @@
+package com.ssaaaxt.f1.test;
+
+import org.junit.Test;
+
+public class ColorTest {
+
+	public String str;
+	
+	@Test
+	public void addStr() {
+		System.out.println(str+"Bug∑÷÷ß");
+	}
+	
+	
+}
